7c1ac00726a15ab2f040c7e4a4acc027a7d02bb5
ccchrischen6 <ccchrischen6@gmail.com>
2020-02-25 16:08:04 -0500

    added collision, random, and interval features

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float lastTime = -0.3f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time-lastTime>0.3)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastTime = Time.time; 
         }
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,24 +9,31 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-
+    private float spawnInterval;
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    //private Random random = new Random();
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        SpawnRandomBall();
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int spawnIdx = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[spawnIdx], spawnPos, ballPrefabs[spawnIdx].transform.rotation);
+
+        spawnInterval = Random.Range(3.0f, 5.0f);
+        Invoke("SpawnRandomBall", spawnInterval);
+        //Debug.Log(spawnInterval);
     }
 
 }
